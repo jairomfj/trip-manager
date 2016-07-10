5a49af5a7c5d8d1156aa635c47a6e7276d42ab0f
Ivan Assalim <ivan.assalim@gmail.com>
2016-07-10 14:22:22 -0300

    adicao de DatabaseConfig

---
+++ src/main/java/br/com/tripmanager/config/DatabaseConfig.java
@@ -0,0 +1,69 @@
+package br.com.tripmanager.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {"br.com.tripmanager.domain"})
+@EnableTransactionManagement
+public class DatabaseConfig {
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private DataSource dataSource;
+
+    private LocalContainerEntityManagerFactoryBean entityManagerFactory;
+
+    @Bean(name = "dataSource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setPackagesToScan(env.getProperty("entitymanager.packagesToScan"));
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+
+        Properties additionalProperties = new Properties();
+        additionalProperties.put("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+        additionalProperties.put("hibernate.show_sql", env.getProperty("spring.jpa.hibernate.show_sql"));
+        additionalProperties.put("hibernate.hbm2dll.auto", env.getProperty("spring.jpa.hibernate.hbm2dll.auto"));
+        entityManagerFactory.setJpaProperties(additionalProperties);
+
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
+        return transactionManager;
+    }
+
+    @Bean
+    public PersistenceAnnotationBeanPostProcessor exceptionTranslator() {
+        return new PersistenceAnnotationBeanPostProcessor();
+    }
+}
