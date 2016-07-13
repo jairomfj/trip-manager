1a30dc75c0fe805344699525f8fee86a6a7f385f
Ivan Assalim <ivan.assalim@gmail.com>
2016-07-12 23:02:45 -0300

    Implementando materialize

--- src/main/java/br/com/tripmanager/controller/LoginController.java
+++ src/main/java/br/com/tripmanager/controller/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
     @RequestMapping("/")
     public String index() {
-        return "index";
+        return "login";
     }
 }
